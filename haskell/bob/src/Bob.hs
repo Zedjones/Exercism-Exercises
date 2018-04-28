b290e93881ceea2d3d62abd4e16338b2b373d773
Nicholas <dojoman19@gmail.com>
2018-04-28 00:50:50 -0400

    Added more stuff to Bob

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -1,4 +1,32 @@
-module Bob (responseFor) where
+module Bob (responseFor, main) where
+
+import Data.Char as Char
+
+main = print(isYelling "1, 2, 3")
+
+isYelling :: String -> Bool 
+isYelling xs = all Char.isUpper xs
 
 responseFor :: String -> String
-responseFor xs = error "You need to implement this function."
+responseFor xs = 
+    if xs == []
+        then "Fine. Be that way!"
+    else if elem '!' xs && elem '?' xs 
+        then "Calm down, I know what I'm doing!"
+    else if elem '!' xs || isYelling xs 
+        then "Whoa, chill out!"
+    else if elem '?' xs
+        then "Sure." 
+    else "Whatever."
+
+substring :: String -> String -> Bool 
+substring (x:xs) [] = False 
+substring xs ys 
+        | prefix xs ys = True 
+        | substring xs (tail ys) = True 
+        | otherwise = False
+
+prefix :: String -> String -> Bool
+prefix [] ys = True
+prefix (x:xs) [] = False
+prefix (x:xs) (y:ys) = (x == y) && prefix xs ys
