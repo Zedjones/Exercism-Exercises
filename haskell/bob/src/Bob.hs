2a2eb39c8e19c696f676df1f23dd52875e7c03a8
Nicholas <dojoman19@gmail.com>
2018-04-28 01:17:29 -0400

    Finished Bob implementation

--- haskell/bob/src/Bob.hs
+++ haskell/bob/src/Bob.hs
@@ -2,20 +2,33 @@ module Bob (responseFor, main) where
 
 import Data.Char as Char
 
-main = print(isYelling "1, 2, 3")
+main = print(last "Ending with ? means a question." == '?')
 
 isYelling :: String -> Bool 
-isYelling xs = all Char.isUpper xs
+isYelling xs = all isYell xs
+
+oneLetter :: String -> Bool
+oneLetter xs = any Char.isLetter xs
+
+isYell :: Char -> Bool 
+isYell c = Char.isUpper c || not (Char.isLetter c)
+
+strip :: String -> String 
+strip = f . f 
+    where f = reverse . dropWhile isSpace
+
+allSpace :: String -> Bool
+allSpace xs = all Char.isSpace xs
 
 responseFor :: String -> String
 responseFor xs = 
-    if xs == []
+    if allSpace xs
         then "Fine. Be that way!"
-    else if elem '!' xs && elem '?' xs 
+    else if isYelling xs && last(strip xs) == '?' && oneLetter xs
         then "Calm down, I know what I'm doing!"
-    else if elem '!' xs || isYelling xs 
+    else if isYelling xs && oneLetter xs
         then "Whoa, chill out!"
-    else if elem '?' xs
+    else if last(strip xs) == '?'
         then "Sure." 
     else "Whatever."
 
