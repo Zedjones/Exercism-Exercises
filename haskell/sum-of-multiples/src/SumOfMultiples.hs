2fcd4d15b13e1c791496b453ac018c3dd16a11a3
Nicholas Jones <dojoman19@gmail.com>
2018-04-30 13:57:17 -0400

    Finished sum of multiples problem

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,4 +1,11 @@
 module SumOfMultiples (sumOfMultiples) where
 
+isMult :: [Integer] -> Integer -> Bool 
+isMult factors numb =
+    any eachFactor factors
+    where eachFactor num = numb `mod` num == 0  
+
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = error "You need to implement this function."
+sumOfMultiples factors limit = 
+    sum $ filter isMultFactors [1 .. limit-1]
+    where isMultFactors n = isMult factors n
