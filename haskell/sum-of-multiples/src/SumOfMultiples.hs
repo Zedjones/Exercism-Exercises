95502a2f4197fdbb39436084d4fcc43f8bf9b2f6
Nicholas Jones <dojoman19@gmail.com>
2018-04-30 14:07:49 -0400

    Moved some things to be one-liners

--- haskell/sum-of-multiples/src/SumOfMultiples.hs
+++ haskell/sum-of-multiples/src/SumOfMultiples.hs
@@ -1,9 +1,7 @@
 module SumOfMultiples (sumOfMultiples) where
 
 isMult :: [Integer] -> Integer -> Bool 
-isMult factors numb =
-    any ((==0) . (numb `mod`)) factors
+isMult factors numb = any ((==0) . (numb `mod`)) factors
 
 sumOfMultiples :: [Integer] -> Integer -> Integer
-sumOfMultiples factors limit = 
-    sum $ filter (isMult factors) [1 .. limit-1]
+sumOfMultiples factors limit = sum $ filter (isMult factors) [1 .. limit-1]
