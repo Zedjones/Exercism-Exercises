eb935efa3587ac2b8006f944eb30210186e0b404
Nicholas <dojoman19@gmail.com>
2018-05-01 11:03:41 -0400

    Cleaned up the code and made more efficient

--- haskell/perfect-numbers/src/PerfectNumbers.hs
+++ haskell/perfect-numbers/src/PerfectNumbers.hs
@@ -1,5 +1,5 @@
 module PerfectNumbers (classify, Classification(..)) where
-
+    
 data Classification = Deficient | Perfect | Abundant deriving (Eq, Show)
 
 isFactor :: Int -> Int -> Bool 
@@ -7,13 +7,12 @@ isFactor factor num = 0 == num `mod` factor
 
 classifyHelp :: Int -> Int -> Maybe Classification
 classifyHelp sum num
-    | num <= 0 = Nothing 
     | sum < num = Just Deficient 
     | sum > num = Just Abundant
     | otherwise = Just Perfect 
 
 classify :: Int -> Maybe Classification
 classify num = 
-    if num < 0 then Nothing
-    else let aliquotSum = sum $ filter (flip isFactor num) [1 .. num-1]
+    if num <= 0 then Nothing
+    else let aliquotSum = sum $ filter (flip isFactor num) [1 .. num `div` 2]
         in classifyHelp aliquotSum num
