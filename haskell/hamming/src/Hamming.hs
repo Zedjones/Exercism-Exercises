c9d539fa8fc801970063a445a15b2e8acf5476a5
Nicholas Jones <dojoman19@gmail.com>
2018-05-01 16:45:57 -0400

    Finished Hamming DNA problem

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -3,4 +3,4 @@ module Hamming (distance) where
 distance :: String -> String -> Maybe Int
 distance xs ys
     | not (length xs == length ys) = Nothing
-    | otherwise = Just $ length [x | x <- xs, y <- ys, x /= y]
+    | otherwise = Just $ length [y | y <- zip xs ys, fst y /= snd y]
