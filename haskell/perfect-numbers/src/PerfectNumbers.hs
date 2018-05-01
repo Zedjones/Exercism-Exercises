5dd8cd4850c46a1bb4d0a48a3c9aeb395c636213
Nicholas <dojoman19@gmail.com>
2018-05-01 10:28:53 -0400

    Added aliquot sum classification program

---
+++ haskell/perfect-numbers/src/PerfectNumbers.hs
@@ -0,0 +1,19 @@
+module PerfectNumbers (classify, Classification(..)) where
+
+data Classification = Deficient | Perfect | Abundant deriving (Eq, Show)
+
+isFactor :: Int -> Int -> Bool 
+isFactor factor num = (num `mod` factor) == 0
+
+classifyHelp :: Int -> Int -> Maybe Classification
+classifyHelp sum num
+    | num <= 0 = Nothing 
+    | sum < num = Just Deficient 
+    | sum > num = Just Abundant
+    | otherwise = Just Perfect 
+
+classify :: Int -> Maybe Classification
+classify num = 
+    if num < 0 then Nothing
+    else let aliquotSum = sum (filter (flip isFactor num) [1 .. num-1])
+        in classifyHelp aliquotSum num
