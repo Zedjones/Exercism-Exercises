160f96b946f5f91462a85ca4c508539ccf50e70e
Nicholas Jones <dojoman19@gmail.com>
2018-05-01 16:47:43 -0400

    Fixed slight redundancy

--- haskell/hamming/src/Hamming.hs
+++ haskell/hamming/src/Hamming.hs
@@ -2,5 +2,5 @@ module Hamming (distance) where
 
 distance :: String -> String -> Maybe Int
 distance xs ys
-    | not (length xs == length ys) = Nothing
+    | length xs /= length ys = Nothing
     | otherwise = Just $ length [y | y <- zip xs ys, fst y /= snd y]
