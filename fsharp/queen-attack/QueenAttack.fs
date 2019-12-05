b7b92bef0b6ee7c9549d4d376f97f646beed29db
Zedjones <dojoman19@gmail.com>
2019-12-04 20:40:05 -0500

    Got raindrops working

---
+++ fsharp/queen-attack/QueenAttack.fs
@@ -0,0 +1,14 @@
+﻿module QueenAttack
+
+let create (position: int * int) = 
+    match position with
+    | (x, y) -> 0 <= x && x < 8 && 0 <= y && y < 8
+
+let checkQueens(queen1: int * int) (queen2: int * int) = failwith "Implement"
+
+let canAttack (queen1: int * int) (queen2: int * int) =
+    match (queen1, queen2) with
+    | ((x1, y1), (x2, y2)) ->
+        if y1 = y2 then true
+        else if x1 = x2 then true
+        else false
---
+++ fsharp/raindrops/Raindrops.fs
@@ -0,0 +1,9 @@
+﻿module Raindrops
+
+let isPling (number: int) = if number % 3 = 0 then "Pling" else ""
+let isPlang (number: int) = if number % 5 = 0 then "Plang" else ""
+let isPlong (number: int) = if number % 7 = 0 then "Plong" else ""
+
+let convert (number: int): string = 
+    let res = String.concat "" [isPling(number); isPlang(number); isPlong(number)] in
+        if res = "" then number.ToString() else res
