b2c153911f8ac56055852f9509a54c6c5c1098d3
Zedjones <dojoman19@gmail.com>
2019-12-06 17:34:51 -0500

    Finished clock

--- fsharp/clock/Clock.fs
+++ fsharp/clock/Clock.fs
@@ -3,11 +3,15 @@ module Clock
 type Clock(hours: int, minutes: int) = 
     member this.Hours = hours
     member this.Minutes = minutes
-
+    override this.Equals(other) = 
+        match other with
+        | :? Clock as otherClock -> this.Hours = otherClock.Hours && this.Minutes = otherClock.Minutes
+        | _ -> false
+   
 let fixNegative minutes = if minutes < 0 then 1440 + minutes else minutes
 
 let calculateProper hours minutes = 
-    let totalMinutes = (60 * hours) + minutes |> fixNegative in
+    let totalMinutes = ((60 * hours) + minutes) % 1440 |> fixNegative in
         totalMinutes / 60 % 24, totalMinutes % 60
 
 let create hours minutes = 
@@ -19,4 +23,4 @@ let add(minutes: int) (clock: Clock) =
 let subtract(minutes: int) (clock: Clock) = 
     calculateProper clock.Hours (clock.Minutes - minutes) |> Clock
 
-let display clock = failwith "You need to implement this function."
+let display (clock: Clock) = sprintf "%02i:%02i" clock.Hours clock.Minutes
