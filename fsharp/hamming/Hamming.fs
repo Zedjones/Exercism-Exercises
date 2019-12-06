decaefed4d9cb2f0f804352daa2c088a0784b6a0
Zedjones <dojoman19@gmail.com>
2019-12-05 20:45:33 -0500

    Added hamming distance

---
+++ fsharp/hamming/Hamming.fs
@@ -0,0 +1,11 @@
+﻿module Hamming
+
+let distance (strand1: string) (strand2: string): int option = 
+    let seq1, seq2 = Seq.toList strand1, Seq.toList strand2 in
+        match seq1.Length = seq2.Length with
+        | true -> 
+            List.zip seq1 seq2 |> 
+            List.filter (fun (x, y) -> x.Equals y |> not) |> 
+            List.length |> 
+            Some
+        | false -> None
