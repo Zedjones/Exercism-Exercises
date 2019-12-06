13704b2e86fc690018074b2eba054b8c0ed81a2b
Zedjones <dojoman19@gmail.com>
2019-12-06 10:12:20 -0500

    Started working on clock

--- fsharp/clock/Clock.fs
+++ fsharp/clock/Clock.fs
@@ -4,10 +4,19 @@ type Clock(hours: int, minutes: int) =
     member this.Hours = hours
     member this.Minutes = minutes
 
-let create hours minutes = failwith "You need to implement this function."
+let fixNegative minutes = if minutes < 0 then 1440 + minutes else minutes
 
-let add minutes clock = failwith "You need to implement this function."
+let calculateProper hours minutes = 
+    let totalMinutes = (60 * hours) + minutes |> fixNegative in
+        totalMinutes / 60 % 24, totalMinutes % 60
 
-let subtract minutes clock = failwith "You need to implement this function."
+let create hours minutes = 
+    calculateProper hours minutes |> Clock
+
+let add(minutes: int) (clock: Clock) =
+    calculateProper clock.Hours (clock.Minutes + minutes) |> Clock
+
+let subtract(minutes: int) (clock: Clock) = 
+    calculateProper clock.Hours (clock.Minutes - minutes) |> Clock
 
 let display clock = failwith "You need to implement this function."
