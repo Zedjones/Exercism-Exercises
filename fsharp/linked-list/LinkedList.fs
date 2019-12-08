66454e3ced18fbd197cbdf058499b3ca6a89045d
Zedjones <dojoman19@gmail.com>
2019-12-08 10:59:31 -0500

    Continued working on this

--- fsharp/linked-list/LinkedList.fs
+++ fsharp/linked-list/LinkedList.fs
@@ -1,25 +1,27 @@
 module LinkedList
 
-type Node(objVal) = 
-    let mutable value = objVal
+type Node() =
+    [<DefaultValue>] val mutable value: obj
     [<DefaultValue>] val mutable prev: Node option
     [<DefaultValue>] val mutable next: Node option
 
-type LinkedList = Value of Node | Empty
-
-let mkLinkedList () = Empty
+let mkLinkedList () =
+    let node = Node()
+    node.prev <- Some (Node())
+    node.next <- Some (Node())
+    node
 
 let pop linkedList = failwith "You need to implement this function."
 
 let shift linkedList = failwith "You need to implement this function."
 
-let push newValue linkedList : LinkedList =
-    match linkedList with
-    | Empty -> Value (Node newValue)
-    | Value(list) ->
-        let newList = Node newValue
-        newList.prev <- Some list
-        list.next <- Some newList
-        Value list
+let push newValue (linkedList: Node) =
+    match linkedList.value with
+    | null -> linkedList.value <- newValue
+    | _ ->
+        let mutable newList = Node()
+        newList.value <- newValue
+        newList.prev <- Some linkedList
+        linkedList.next <- Some newList
 
 let unshift newValue linkedList = failwith "You need to implement this function."
