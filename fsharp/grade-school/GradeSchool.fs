d4a61f53a83824b90b7b5e2ea4f7d65958b6522d
Zedjones <dojoman19@gmail.com>
2020-09-22 17:25:32 -0400

    Finished grade school

--- fsharp/grade-school/GradeSchool.fs
+++ fsharp/grade-school/GradeSchool.fs
@@ -4,8 +4,15 @@ type School = Map<int, string list>
 
 let empty: School = Map.empty
 
-let add (student: string) (grade: int) (school: School): School = failwith "You need to implement this function."
+let add (student: string) (grade: int) (school: School): School =
+    school.Add
+        (grade,
+         match school.TryFind grade with
+         | None -> [ student ]
+         | Some (oldGrade) -> List.sort (student :: oldGrade))
 
-let roster (school: School): string list = failwith "You need to implement this function."
+let roster (school: School): string list =
+    List.collect (fun (_, value) -> value) (school |> Map.toList)
 
-let grade (number: int) (school: School): string list = failwith "You need to implement this function."
+let grade (number: int) (school: School): string list =
+    school.TryFind number |> Option.defaultValue []
