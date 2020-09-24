178f12766ed10bfb54cb9fce4da375ad75ae28e2
Zedjones <dojoman19@gmail.com>
2020-09-23 20:23:30 -0400

    Finished Armstrong numbers and difference of squares

---
+++ fsharp/armstrong-numbers/ArmstrongNumbers.fs
@@ -0,0 +1,9 @@
+﻿module ArmstrongNumbers
+
+let isArmstrongNumber (number: int): bool =
+    let numList = number.ToString() |> Seq.toList
+    List.fold (fun acc digit ->
+        acc
+        + (System.Char.GetNumericValue digit
+           ** float numList.Length
+           |> int)) 0 numList = number
---
+++ fsharp/difference-of-squares/DifferenceOfSquares.fs
@@ -0,0 +1,8 @@
+﻿module DifferenceOfSquares
+
+let squareOfSum (number: int): int = pown (List.sum [ 1 .. number ]) 2
+
+let sumOfSquares (number: int): int =
+    List.sumBy (fun x -> pown x 2) [ 1 .. number ]
+
+let differenceOfSquares (number: int): int = squareOfSum number - sumOfSquares number
